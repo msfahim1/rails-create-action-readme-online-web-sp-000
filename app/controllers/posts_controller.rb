9263fa0afdaac476b5b6fbec04005298cd532aeb
Michael Fahim <michael_fahim@yahoo.com>
2020-08-28 00:28:29 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,12 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # add create method here
+  # add create method 
+  def create
+    post = Post.new
+    post.title = "Title Goes Here"
+    post.description = "Desc goes here..."
+    post.save
+
+  end
 end
