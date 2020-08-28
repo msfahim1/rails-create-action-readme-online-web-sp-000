b4da4a42a22b32b0b239d2d4d01ec484a5433fc0
Michael Fahim <michael_fahim@yahoo.com>
2020-08-28 00:40:34 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
     post.title = params[:title]
     post.description = params[:description]
     post.save
+    redirect_to post_path(@post)
 
   end
 end
